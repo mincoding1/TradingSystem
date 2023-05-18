a16afe6089c51afa18463bcef2adc293a6f4c158
mincoding <74457149+mincoding1@users.noreply.github.com>
2023-05-18 21:22:13 +0900

    Update NemoAPI.java

--- java/NemoAPI.java
+++ java/NemoAPI.java
@@ -2,13 +2,13 @@ import java.util.Random;
 import java.time.LocalDateTime;
 
 public class NemoAPI {
-    public void certification( String ID, String Password) {
+    public void certification( String ID, String Pass) {
         System.out.println("[네모]" + ID + "님 로그인 성공");
     }
     public void purchasingStock( String stockCode,int price, int count){
         System.out.println("[네모]" + stockCode + "를 " + price + "가격에 매수하였음");
     }
-    public void ellingStock( String stockCode , int price, int count) {
+    public void sellingStock( String stockCode , int price, int count) {
         System.out.println("[네모]" + stockCode + "를 " + price + "가격에 매도하였음");
     }
     public int getMarketPrice ( String stockCode, int minute) {
