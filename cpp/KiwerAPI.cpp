b2901a1d755626f2c3ade2196c665297b8ea43f9
mincoding <74457149+mincoding1@users.noreply.github.com>
2025-04-14 18:55:57 +0900

    Update KiwerAPI.cpp

--- cpp/KiwerAPI.cpp
+++ cpp/KiwerAPI.cpp
@@ -1,24 +1,24 @@
 #pragma once
 #include <iostream>
+#include <iostream>
 #include <cstdlib>
 #include <string>
 
-using namespace std;
 class KiwerAPI {
 public:
-	void login(string ID, string password) {
-		cout << ID << " login success\n";
+	void login(std::string ID, std::string password) {
+		std::cout << ID << " login success\n";
 	}
 
-	void buy(string stockCode, int count, int price) {
-		cout << stockCode << " : Buy stock ( " << price << " * " << count << ")\n";
+	void buy(std::string stockCode, int count, int price) {
+		std::cout << stockCode << " : Buy stock ( " << price << " * " << count << ")\n";
 	}
 
-	void sell(string stockCode, int count, int price) {
-		cout << stockCode << " : Sell stock ( " << price << " * " << count << ")\n";
+	void sell(std::string stockCode, int count, int price) {
+		std::cout << stockCode << " : Sell stock ( " << price << " * " << count << ")\n";
 	}
 
-	int currentPrice(string stockCode) {
+	int currentPrice(std::string stockCode) {
 		srand(time(NULL));
 		int ret = rand() % 10 * 100 + 5000;
 		return ret;
